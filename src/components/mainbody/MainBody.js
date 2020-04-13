cf8755d853a878eff8571edfd7ddb251d5ecba32
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-12 17:03:15 -0700

    🌋🏔putting the block in place

---
+++ src/components/mainbody/MainBody.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MainBody = () => {
+  return (
+    <div>
+      Welcome to Twin Peaks, the REST API. This API returns two types of
+      data, Character information and Quotes.
+      <p>Please note, the information regarding how to consume this REST API
+      assumes the user is familure with the basic REST API concept. If you would
+      like more information on the basics of this topic, please head over 
+      to <a href="https://restfulapi.net/">https://restfulapi.net/</a> to learn
+      more.</p>
+    </div>
+  );
+};
+
+export default MainBody;
