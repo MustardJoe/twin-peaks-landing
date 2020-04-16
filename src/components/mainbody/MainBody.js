7f4d144a4e7c9d94ea36f201681f14899238b2cf
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-15 23:55:02 -0700

    adding to landing text

--- src/components/mainbody/MainBody.js
+++ src/components/mainbody/MainBody.js
@@ -5,31 +5,57 @@ console.log(styles);
 const MainBody = () => {
   return (
     <div className={styles.mainbody}>
-      Welcome to Twin Peaks, the REST API. This API returns two types of
-      data, Character information and Quotes.
+      Welcome to Twin Peaks, the REST API, the landing page. 
+      
+      <p><span className={styles.invertText}>The API is live at <a href="https://twin-peaks.herokuapp.com/" 
+      >https://twin-peaks.herokuapp.com/</a></span></p>
 
-      <p>The API is live at <a href="https://twin-peaks.herokuapp.com/" 
-      >https://twin-peaks.herokuapp.com/</a>, but you will need to querry
-      the specific routes provided to return data</p>
+      Routes from this API return two types of
+      data, Character information and Quotes. Querry the specific routes
+      provided to return data. The two paths are listed below, each of which
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      provides an array of all of that path's respective data:
 
-      <ul>
-        <li>https://twin-peaks.herokuapp.com/api/v1/characters</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/characters/:id</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/characters/randomCharacter</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/characters/allCharsAllQuotes</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/characters/characterWithQuotes/:id</li>
-      </ul>
-      <ul>
-        <li>https://twin-peaks.herokuapp.com/api/v1/quotes</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/quotes/:id</li>
-        <li>https://twin-peaks.herokuapp.com/api/v1/quotes/randomQuote</li>
-      </ul>
+      <p>
+        <span className={styles.invertText}>basic routes:</span>
+        <ul>
+          <li>https://twin-peaks.herokuapp.com/api/v1/characters</li>
+          <li>https://twin-peaks.herokuapp.com/api/v1/quotes</li>
+        </ul>
+      </p>
 
-      <p>Please note, the information regarding how to consume this REST API
-      assumes the user is familure with the basic REST API concept. If you would
-      like more information on the basics of this topic, please head over 
-      to <a href="https://restfulapi.net/">https://restfulapi.net/</a> to learn
-      more.</p>
+      <p>
+        <span className={styles.invertText}>characters routes</span>
+        <ul>
+          <li>https://twin-peaks.herokuapp.com/api/v1/characters/:id</li>
+          <li>https://twin-peaks.herokuapp.com/api/v1/characters/randomCharacter</li>
+          <li>https://twin-peaks.herokuapp.com/api/v1/characters/allCharsAllQuotes</li>
+          <li>https://twin-peaks.herokuapp.com/api/v1/characters/characterWithQuotes/:id</li>
+        </ul>
+      </p>
+
+      <p>
+        <span className={styles.invertText}>quotes routes:</span>
+        <ul>
+          <li>https://twin-peaks.herokuapp.com/api/v1/quotes/:id</li>
+          <li>https://twin-peaks.herokuapp.com/api/v1/quotes/randomQuote</li>
+        </ul>
+      </p>
+
+      <p>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        *The routes ending in ':id' require a character or quote id (the id supplied by the 
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        MongoDB in the '_id' field for each character and quote entry). 
+      </p>
+
+      <p>
+        Please note, the information regarding how to consume this site
+        assumes the user is familure with the basic REST API concept. If you would
+        like more information on the basics of this topic, please head over 
+        to <a href="https://restfulapi.net/">https://restfulapi.net/</a> to learn
+        more.
+      </p>
     </div>
   );
 };
