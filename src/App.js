87c283075ce0c42d9df0784f9fc46680756ed37d
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-16 17:51:25 -0700

    🦐added in footer comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header/Header';
 import MainBody from './components/mainbody/MainBody';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Header />
       </header>
       <MainBody />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      Thanks for visiting, and please check out more of my projects on 
+      <a href="https://www.jonforney.com" target="_blank" rel="noopener noreferrer">
+        https://www.jonforney.com
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
