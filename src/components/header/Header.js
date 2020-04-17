716894b26fcd82d43257a1b36efb4b4ba4479746
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-16 21:36:46 -0700

    made header a link to home

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,10 @@ import styles from './header.module.css';
 const Header = () => {
   return (
     <div>
-      <img className={styles.img}
-        src={headerImg} alt="Welcome to Twin Peaks, the REST API"></img>
+      <a href="./">
+        <img className={styles.img}
+          src={headerImg} alt="Welcome to Twin Peaks, the REST API"></img>
+      </a>
     </div>
   );
 };
