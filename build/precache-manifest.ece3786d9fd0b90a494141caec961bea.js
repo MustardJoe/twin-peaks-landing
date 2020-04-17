693acbe01076c0de106d787dcd805f19d9b0f2a3
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-16 23:08:54 -0700

    adding build

---
+++ build/precache-manifest.ece3786d9fd0b90a494141caec961bea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca6d50b66954c99ecdce5ed41b832b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d29e35270cc06a515b35",
+    "url": "/static/css/main.6d8d4ebe.chunk.css"
+  },
+  {
+    "revision": "21779acfa78507324b67",
+    "url": "/static/js/2.ef6c0b71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef6c0b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29e35270cc06a515b35",
+    "url": "/static/js/main.21e56681.chunk.js"
+  },
+  {
+    "revision": "6a28de6ed8fd96c18c8f",
+    "url": "/static/js/runtime-main.0abe76fc.js"
+  },
+  {
+    "revision": "ebcc492031aa17a34ea0176c445c93ae",
+    "url": "/static/media/blackwhitezigraw.ebcc4920.png"
+  },
+  {
+    "revision": "04f4a24c465add17f3da799e07233faf",
+    "url": "/static/media/twinpeaksheader.04f4a24c.png"
+  }
+]);
